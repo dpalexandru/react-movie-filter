371ad5f090376d505f8b8753f4ef269a30b9f181
dpalexandru <alexpoian91@hotmail.com>
2025-09-01 15:15:28 +0200

    feat: add a static ul from array data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
 import { useState, useEffect } from 'react'
 
 
+// D A T A 
+
+const movies = [
+  { title: 'Inception', genre: 'Fantascienza' },
+  { title: 'Il Padrino', genre: 'Thriller' },
+  { title: 'Titanic', genre: 'Romantico' },
+  { title: 'Batman', genre: 'Azione' },
+  { title: 'Interstellar', genre: 'Fantascienza' },
+  { title: 'Pulp Fiction', genre: 'Thriller' },
+]
+
+
 function App() {
 
   return (
     <>
-      <h1>Hello word</h1>
+      <h1>Lista dei film da vedere una volta nella vita</h1>
+      <ul className="list-group">
+        {movies.map((movie, index) => (
+          <li
+            key={index}
+            className='list-group-item'
+          >
+            {movie.title} <br />
+            <span className='text-muted small'> Genere : {movie.genre}</span>
+          </li>
+        ))}
+
+      </ul>
+
 
     </>
   )
